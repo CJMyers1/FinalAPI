72b72f50c4f80f0e09a0a0973ed384ab77ffbac0
CJMyers1 <myerscasper73@gmail.com>
2022-02-24 09:20:49 -0700

    Final API

---
+++ math.random.js
@@ -0,0 +1,11 @@
+document.getElementById("charge").addEventListener("click", withdrawal());
+document.getElementById("payment").addEventListener("click", deposit());
+
+let withdrawals = [];
+
+// withdrawal function
+function withdrawal() {
+    withdrawals.forEach((charge) => {
+        const { company, amount } = charge;
+    })
+}
